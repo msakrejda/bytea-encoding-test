28bbb9d688ce7545776be808b9a0fb336eefc605
Maciek Sakrejda <maciek@heroku.com>
2015-05-12 16:52:53 -0700

    Configure AttrVault for ByteaThings

--- lib/models/bytea_thing.rb
+++ lib/models/bytea_thing.rb
@@ -1,10 +1,18 @@
 class ByteaThing < Sequel::Model
   plugin :timestamps
 
+  include AttrVault
+
+  vault_keyring ENV['ATTR_VAULT_KEYRING']
+  vault_attr :stuff
 end
 
 class NineOneByteaThing < Sequel::Model(:bytea_things)
   self.db = NINE_ONE_DB
   plugin :timestamps
 
+  include AttrVault
+
+  vault_keyring ENV['ATTR_VAULT_KEYRING']
+  vault_attr :stuff
 end
