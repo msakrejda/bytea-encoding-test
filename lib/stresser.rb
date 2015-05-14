1376e0dd176fcc76d47b9d50620a20073626c491
Maciek Sakrejda <maciek@heroku.com>
2015-05-14 09:37:13 -0700

    Open and close connections concurrently

--- lib/stresser.rb
+++ lib/stresser.rb
@@ -12,6 +12,15 @@ class Stresser
     "postgres://#{random_letters}:#{random_letters}@example.com/#{random_letters}"
   end
 
+  def self.test_bytea_encoding(db_url, len=32)
+    Sequel.connect(db_url) do |c|
+      if actual = c.fetch("SELECT :bytes::bytea AS bytes",
+                          bytes: Sequel.blob(Random.new.bytes(len))).first[:bytes].length != len
+        fail "Inconsistent bytea length: expected #{len}; got #{actual}"
+      end
+    end
+  end
+
   def self.run_batch
     lock = Mutex.new
     active = true
@@ -21,8 +30,18 @@ class Stresser
         sleep 0.05
       end
     end
-
     rand = Random.new
+    sequel_thread = Thread.new do
+      while lock.synchronize { active } do
+        test_bytea_encoding(ENV['DATABASE_URL'])
+      end
+    end
+    sequel91_thread = Thread.new do
+      while lock.synchronize { active } do
+        test_bytea_encoding(ENV['NINE_ONE_DATABASE_URL'])
+      end
+    end
+
     [ ByteaThing, NineOneByteaThing ].map do |klazz|
       Thread.new do
         1000.times do
@@ -47,6 +66,6 @@ class Stresser
       klazz.dataset.delete
     end
     lock.synchronize { active = false }
-    http_thread.join
+    [ http_thread, sequel_thread, sequel91_thread ].each(&:join)
   end
 end
